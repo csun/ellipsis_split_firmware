a8b2dd3eb70ecc7abd78c7b09c33a7f777e8eddb
Cameron Sun <me@csun.io>
2020-06-14 00:21:00 -0700

    Seems finished

--- keyboards/ellipsis_split/keymaps/default/keymap.c
+++ keyboards/ellipsis_split/keymaps/default/keymap.c
@@ -4,8 +4,8 @@
 
 enum layers {
 	_WIN = 0,
-	_MAC,
 	_LOWER_WIN,
+	_MAC,
 	_LOWER_MAC,
 	_NUM,
 	_GAME,
@@ -28,30 +28,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	),
 	[_LOWER_WIN] = LAYOUT_all(
 		KC_TRNS,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,        KC_F6,                           KC_NUM,  KC_F7,    KC_F8,        KC_F9,    KC_F10,    KC_TRNS, KC_TRNS,  KC_TRNS,
-		KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,        TO(_MAC), KC_TRNS, KC_TRNS,    KC_TRNS,        KC_TRNS,    KC_TRNS,    KC_TRNS,    LCTL(KC_PGUP), LCTL(KC_PGDN), KC_TRNS,
+		KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,        TG(_MAC), KC_TRNS, KC_TRNS,    KC_TRNS,        KC_TRNS,    KC_TRNS,    KC_TRNS,    LCTL(KC_PGUP), LCTL(KC_PGDN), KC_TRNS,
 		KC_CAPS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,        KC_CYCLE_SPACE, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT,    KC_TRNS, KC_TRNS, KC_TRNS,  KC_AUDIO_VOL_UP,
 		KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,        KC_TRNS,  KC_TRNS, KC_TRNS,   KC_TRNS,        KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,   KC_AUDIO_VOL_DOWN,
 		KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_MEDIA_PREV_TRACK, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK
 	),
 	[_MAC] = LAYOUT_all(
-		KC_ESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,   KC_GRV,  KC_7,    KC_8,        KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
-		KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_PGUP, KC_ESC, KC_Y,    KC_U,        KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-		KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_END, KC_HOME, KC_H,    KC_J,        KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP,
-		KC_LSFT,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_PGDN,  KC_DEL, KC_N,   KC_M,        KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
-		KC_LCTL,  KC_LALT, KC_LGUI, KC_RAISE, KC_LSFT, MO(_LOWER_MAC), KC_ENT, KC_SPC, KC_BSPC,  MO(_LOWER_MAC), KC_RCTL, KC_LEFT, KC_DOWN, KC_RIGHT
+		KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,        KC_TRNS,   KC_TRNS,  KC_TRNS,    KC_TRNS,        KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS,
+		KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,        KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,        KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
+		KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,        KC_TRNS,  KC_TRNS, KC_TRNS,   KC_TRNS,        KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,
+		KC_TRNS,  KC_LALT, KC_LGUI, KC_TRNS, KC_TRNS, MO(_LOWER_MAC), KC_TRNS, KC_TRNS, KC_TRNS,  MO(_LOWER_MAC), KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 	),
 	[_LOWER_MAC] = LAYOUT_all(
 		KC_TRNS,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,        KC_F6,                           KC_NUM,  KC_F7,    KC_F8,        KC_F9,    KC_F10,    KC_TRNS, KC_TRNS,  KC_TRNS,
-		KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,        TO(_WIN), KC_TRNS, KC_TRNS,    KC_TRNS,        KC_TRNS,    KC_TRNS,    KC_TRNS, LGUI(LSFT(KC_LBRC)), LGUI(LSFT(KC_RBRC)), KC_TRNS,
+		KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,        TG(_MAC), KC_TRNS, KC_TRNS,    KC_TRNS,        KC_TRNS,    KC_TRNS,    KC_TRNS, LGUI(LSFT(KC_LBRC)), LGUI(LSFT(KC_RBRC)), KC_TRNS,
 		KC_CAPS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,        KC_CYCLE_SPACE, KC_TRNS, KC_LEFT,    KC_DOWN,        KC_UP,    KC_RIGHT,    KC_TRNS, KC_TRNS, KC_TRNS,  KC__VOLUP,
 		KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,        KC_TRNS,  KC_TRNS, KC_TRNS,   KC_TRNS,        KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,   KC__VOLDOWN,
 		KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_MEDIA_PREV_TRACK, KC_MEDIA_PLAY_PAUSE, KC_MEDIA_NEXT_TRACK
 	),
 	[_NUM] = LAYOUT_all(
-		KC_TRNS,   KC_KP_1,    KC_KP_2,    KC_KP_3,    KC_KP_4,    KC_KP_5,        KC_KP_6,   KC_TRNS,  KC_KP_7,    KC_KP_8,        KC_KP_9,    KC_KP_0,    KC_TRNS, KC_TRNS,  KC_TRNS,
-		KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,        KC_TRNS,    KC_TRNS,    KC_TRNS,    LSFT(LGUI(KC_LBRC)), LSFT(LGUI(KC_RBRC)), KC_TRNS,
-		KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS,        KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
-		KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,        KC_TRNS,  KC_TRNS, KC_TRNS,   KC_TRNS,        KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,
+		KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,        KC_TRNS,   KC_TRNS,  KC_TRNS,    KC_TRNS,        KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS,  KC_TRNS,
+		KC_TRNS,   KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,        KC_TRNS, KC_TRNS, KC_KP_1,    KC_KP_2,        KC_KP_3,    KC_TRNS,    KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,
+		KC_TRNS, KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,        KC_TRNS, KC_TRNS, KC_KP_4,    KC_KP_5,        KC_KP_6,    KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,
+		KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,    KC_TRNS,        KC_TRNS,  KC_TRNS, KC_KP_7,   KC_KP_8,        KC_KP_9, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS,
 		KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 	),
 	[_GAME] = LAYOUT_all(
@@ -169,11 +169,11 @@ void draw_raw(const char *data, uint16_t start) {
 }
 
 void draw_base(uint16_t start) {
-	if(layer_state_is(_WIN)) {
-		draw_raw(gfx_win, start);
+	if(layer_state_is(_MAC)) {
+		draw_raw(gfx_mac, start);
 	}
 	else {
-		draw_raw(gfx_mac, start);
+		draw_raw(gfx_win, start);
 	}
 }
 
@@ -259,7 +259,7 @@ bool process_cycle_space(uint16_t keycode, keyrecord_t *record) {
 bool process_raise(uint16_t kc, keyrecord_t *record) {
 	if(kc == KC_RAISE) {
 		if(record->event.pressed) {
-			raise_mod = layer_state_is(_WIN) ? MOD_LCTL : MOD_LGUI;
+			raise_mod = layer_state_is(_MAC) ? MOD_LGUI : MOD_LCTL;
 			register_mods(raise_mod);
 		}
 		else {
@@ -272,19 +272,7 @@ bool process_raise(uint16_t kc, keyrecord_t *record) {
 	if (raise_mod) {
 		uint8_t new_mod = raise_mod;
 
-		if(layer_state_is(_WIN)) {
-			if (kc == KC_Q) {
-				tap_code16(LALT(KC_F4));
-				return false;
-			}
-			else if (kc == KC_TAB || kc == KC_SPC) {
-				new_mod = MOD_LALT;
-			}
-			else {
-				new_mod = MOD_LCTL;
-			}
-		}
-		else {
+		if(layer_state_is(_MAC)) {
 			if (kc == KC_SPC ||
 				kc == KC_BSPC ||
 				kc == KC_LEFT ||
@@ -295,6 +283,18 @@ bool process_raise(uint16_t kc, keyrecord_t *record) {
 			}
 			else {
 				new_mod = MOD_LGUI;
+			}
+		}
+		else {
+			if (kc == KC_Q) {
+				tap_code16(LALT(KC_F4));
+				return false;
+			}
+			else if (kc == KC_TAB || kc == KC_SPC) {
+				new_mod = MOD_LALT;
+			}
+			else {
+				new_mod = MOD_LCTL;
 			}
 		}
 
